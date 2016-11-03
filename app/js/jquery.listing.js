f625c2f4a9ac8f4023ba37f16e18a97326e14b4d
petruninevgeniy <petruninevgeniy@gmail.com>
2016-11-03 09:18:13 +0200

    creat listing page

--- app/js/jquery.listing.js
+++ app/js/jquery.listing.js
@@ -9,7 +9,72 @@
 
         } );
 
+        $.each( $( '.listing__catalog-list' ), function() {
+
+            new HeightBlock ( $( this ) );
+
+        } );
+
     });
+
+    var HeightBlock = function( obj ) {
+
+        //private properties
+        var _self = this,
+            _obj = obj,
+            _header = $( '.site__header' ),
+            _filter = $( '.listing__filter' ),
+            _sort = $( '.listing__catalog-sort' ),
+            _listItem = $('.listing__catalog-item'),
+            _map = $('.listing__map'),
+            _window = $( window ),
+            _scroll = null;
+
+        //private methods
+        var _onEvents = function() {
+
+
+            },
+            _setHeight = function () {
+
+                var windowHeight = _window.height(),
+                    headerHeight = _header.height(),
+                    filterHeight = _filter.outerHeight( true ),
+                    sortHeight = _sort.outerHeight( true ),
+                    marginItem = parseFloat(_listItem.css('margin-bottom'));
+
+                console.log(headerHeight, filterHeight, sortHeight, marginItem);
+
+                _obj.css({
+                    height: windowHeight - ( headerHeight + filterHeight + sortHeight + marginItem )
+                });
+
+                _addScroll()
+            },
+            _setMapPosition = function () {
+
+                var filterHeight = _filter.outerHeight( true ),
+                    sortHeight = _sort.outerHeight( true );
+
+                _map.css({
+                    top: filterHeight + sortHeight
+                });
+
+            },
+            _addScroll = function () {
+
+                _scroll = _obj.perfectScrollbar();
+
+            },
+            _init = function() {
+                _obj[ 0 ].obj = _self;
+                _onEvents();
+                _setHeight();
+                _setMapPosition();
+            };
+
+        _init();
+    };
 
     var FilterListing = function( obj ) {
 
--- app/js/jquery.map.js
+++ app/js/jquery.map.js
@@ -26,10 +26,11 @@
             },
             _initMap = function () {
                 _map = new google.maps.Map( _obj[ 0 ], {
-                    scrollwheel: false,
+                    // scrollwheel: false,
                     draggable: !("ontouchend" in document),
                     zoom: 10,
-                    center: { "lat": 50.841220, "lng": 4.355132 }
+                    center: { "lat": 50.841220, "lng": 4.355132 },
+                    styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#cfd8da"},{"visibility":"on"}]}]
                 });
 
             },
